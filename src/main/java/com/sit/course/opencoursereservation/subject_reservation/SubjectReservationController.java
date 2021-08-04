21fab15cd0f64efcca2e02aa08ef715ba76cca55
bazsup <bas10600@gmail.com>
2021-08-04 13:28:50 +0700

    add test controller

--- src/main/java/com/sit/course/opencoursereservation/subject_reservation/SubjectReservationController.java
+++ src/main/java/com/sit/course/opencoursereservation/subject_reservation/SubjectReservationController.java
@@ -7,10 +7,7 @@ import com.sit.course.opencoursereservation.user.repository.UserRepository;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Setter
 @RestController
@@ -34,5 +31,11 @@ public class SubjectReservationController {
         User mockUser = userRepository.findUserById(1L);
 
         return subjectReservationService.reserveSubject(subjectId, mockUser);
+    }
+
+    @GetMapping("/test")
+    @ResponseStatus(HttpStatus.OK)
+    public String test() {
+        return "Hello, Test.";
     }
 }
